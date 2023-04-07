5e86bceea2f620a1c62164c25e81de3bf4c39b24
tenxmacbookuser <adeel_ahmed_19@hotmail.com>
2023-04-07 10:36:26 +0500

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/imagegalleryproject/fragments/ImageViewerFragment.kt
+++ app/src/main/java/com/example/imagegalleryproject/fragments/ImageViewerFragment.kt
@@ -3,6 +3,7 @@ package com.example.imagegalleryproject.fragments
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -11,10 +12,13 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.example.imagegalleryproject.databinding.FragmentImageViewerBinding
+import com.example.imagegalleryproject.model.FavoriteImage
+import com.example.imagegalleryproject.viewmodel.FavoriteViewModel
 
 
 class ImageViewerFragment : Fragment() {
     lateinit var binding: FragmentImageViewerBinding
+    private lateinit var favoriteViewModel: FavoriteViewModel
     private val args: ImageViewerFragmentArgs by navArgs()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,6 +39,8 @@ class ImageViewerFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentImageViewerBinding.inflate(inflater, container, false)
 
+        favoriteViewModel = FavoriteViewModel(requireActivity().application)
+
         (requireActivity() as AppCompatActivity).supportActionBar?.title = "View Image"
 
         requireActivity().supportFragmentManager
@@ -42,6 +48,11 @@ class ImageViewerFragment : Fragment() {
         val imageRes = args.imageRes
 
         Glide.with(binding.ivDialog).load(imageRes).into(binding.ivDialog)
+
+        binding.addFav.setOnClickListener {
+            favoriteViewModel.addFavorites(FavoriteImage(imageRes))
+            Toast.makeText(requireContext(), "Image Added to Favorites", Toast.LENGTH_SHORT).show()
+        }
 
         return binding.root
     }
